a550f01142846069faeb54d1cbb0b0be8b9f653a
tomer <tomer@DESKTOP-OH6ANAJ>
2018-12-06 21:43:36 +0200

    Working 12/06 - 21:45

--- AT Modem Proj/src/Window2.java
+++ AT Modem Proj/src/Window2.java
@@ -1,10 +1,8 @@
 import javax.swing.*;
+import javax.swing.text.DefaultCaret;
 
-import com.fazecast.jSerialComm.SerialPort;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import com.fazecast.jSerialComm.*;
+import java.awt.event.*;
 
 @SuppressWarnings({"unused", "serial"})
 public class Window2 extends JFrame{
@@ -18,21 +16,47 @@ public class Window2 extends JFrame{
  private int index = 0;
  private static SerialPort PortChosen;
  
-	private static enum State{
+private static enum State{
 		Idle, TypingNumber, TypingMessage, Dialing, Ringing, DuringCall;
 	}
 	
 	static State PhoneState = State.Idle;
 	private static JTextArea textArea;
+	private JButton Answer;
 	
 public Window2(){ 
 		
 	 JPanel p = new JPanel();
 	 textArea = new JTextArea();
-     textArea.setBounds(270, 30, 284, 67);
+     textArea.setBounds(270, 30, 141, 35);
      p.add(textArea);
      
-     ImageIcon image2 = new ImageIcon("C:/Users/tomer/Documents/Nokia2.jpg");
+     JScrollPane scrollPane = new JScrollPane();
+     scrollPane.setBounds(270, 205, 243, 145);
+     p.add(scrollPane);
+     
+     JTextArea textArea_Debug = new JTextArea();
+     scrollPane.setViewportView(textArea_Debug);
+     DefaultCaret caret = (DefaultCaret)textArea_Debug.getCaret();
+	 caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+	 scrollPane.setViewportView(textArea_Debug);
+           
+     
+     ImageIcon image1 = new ImageIcon("C:/Users/tomer/Documents/Nokia1.png");
+     JButton button1 = new JButton("");
+     button1.setIcon(image1);
+     button1.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent arg0) {
+     		CreatingNumber("1");
+     	}
+     });
+     p.setLayout(null);
+     button1.setBounds(50,300,60,50);
+     p.add(button1);
+
+
+ImageIcon image2 = new ImageIcon("C:/Users/tomer/Documents/Nokia2.jpg");
 	  JButton button2 = new JButton("");
 	  button2.setIcon(image2);
 	  button2.addMouseListener(new MouseAdapter() {
@@ -41,132 +65,217 @@ public Window2(){
 	  		CreatingNumber("2");
 	  	}
 	  });
-      p.setLayout(null);
-      button2.setBounds(110,300,60,50);
-      p.add(button2);
+     p.setLayout(null);
+     button2.setBounds(110,300,60,50);
+     p.add(button2);
 	 
-      ImageIcon image5 = new ImageIcon("C:/Users/tomer/Documents/Nokia5.jpg");
-      JButton button5 = new JButton("");
-      button5.setIcon(image5);
-      button5.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		CreatingNumber("5");
-      	}
-      });
-      p.setLayout(null);
-      button5.setBounds(110,350,60,50);
-      p.add(button5);
-      
-      ImageIcon image8 = new ImageIcon("C:/Users/tomer/Documents/Nokia8.jpg");
-      JButton button8 = new JButton("");
-      button8.setIcon(image8);
-      button8.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		CreatingNumber("8");
-      	}
-      });
-      p.setLayout(null);
-      button8.setBounds(110,400,60,50);
-      p.add(button8);
-      
-      ImageIcon image0 = new ImageIcon("C:/Users/tomer/Documents/Nokia0.jpg");
-      JButton button0 = new JButton("");
-      button0.setIcon(image0);
-      button0.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		CreatingNumber("0");
-      	}
-      });
-      p.setLayout(null);
-      button0.setBounds(110,450,60,50);
-      p.add(button0);
-      
-      ImageIcon image1 = new ImageIcon("C:/Users/tomer/Documents/Nokia1.png");
-      JButton button1 = new JButton("");
-      button1.setIcon(image1);
-      button1.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent arg0) {
-      		CreatingNumber("1");
-      	}
-      });
-      p.setLayout(null);
-      button1.setBounds(50,300,60,50);
-      p.add(button1);
-      
-      ImageIcon image4 = new ImageIcon("C:/Users/tomer/Documents/Nokia4.jpg");
-      JButton button4 = new JButton("");
-      button4.setIcon(image4);
-      button4.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		CreatingNumber("4");
-      	}
-      });
-      p.setLayout(null);
-      button4.setBounds(50,350,60,50);
-      p.add(button4);
-      
-      ImageIcon image7 = new ImageIcon("C:/Users/tomer/Documents/Nokia7.jpg");
-      JButton button7 = new JButton("");
-      button7.setIcon(image7);
-      button7.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		CreatingNumber("7");
-      	}
-      });
-      p.setLayout(null);
-      button7.setBounds(50,400,60,50);
-      p.add(button7);
-      
-      ImageIcon image3 = new ImageIcon("C:/Users/tomer/Documents/Nokia3.jpg");
-      JButton button3 = new JButton("");
-      button3.setIcon(image3);
-      button3.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		CreatingNumber("3");
-      	}
-      });
-      p.setLayout(null);
-      button3.setBounds(170,300,60,50);
-      p.add(button3);
-      
-      ImageIcon image6 = new ImageIcon("C:/Users/tomer/Documents/Nokia6.jpg");
-      JButton button6 = new JButton("");
-      button6.setIcon(image6);
-      button6.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		CreatingNumber("6");
-      	}
-      });
-      p.setLayout(null);
-      button6.setBounds(170,350,60,50);
-      p.add(button6);
-      
-      ImageIcon image9 = new ImageIcon("C:/Users/tomer/Documents/Nokia9.jpg");
-      JButton button9 = new JButton("");
-      button9.setIcon(image9);
-      button9.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		CreatingNumber("9");
-      	}
-      });
-      p.setLayout(null);
-      button9.setBounds(170,400,60,50);
-      p.add(button9);
-      
-      getContentPane().add(p);
-      
+     
+ImageIcon image3 = new ImageIcon("C:/Users/tomer/Documents/Nokia3.jpg");	 
+	JButton button3 = new JButton("");
+     button3.setIcon(image3);
+     button3.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		CreatingNumber("3");
+     	}
+     });
+     p.setLayout(null);
+     button3.setBounds(170,300,60,50);
+     p.add(button3);
+     
+	  
+ImageIcon image4 = new ImageIcon("C:/Users/tomer/Documents/Nokia4.jpg");
+     JButton button4 = new JButton("");
+     button4.setIcon(image4);
+     button4.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		CreatingNumber("4");
+     	}
+     });
+     p.setLayout(null);
+     button4.setBounds(50,350,60,50);
+     p.add(button4);
+     
+	  
+ImageIcon image5 = new ImageIcon("C:/Users/tomer/Documents/Nokia5.jpg");
+     JButton button5 = new JButton("");
+     button5.setIcon(image5);
+     button5.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		CreatingNumber("5");
+     	}
+     });
+     p.setLayout(null);
+     button5.setBounds(110,350,60,50);
+     p.add(button5);
+     
+	  
+ImageIcon image6 = new ImageIcon("C:/Users/tomer/Documents/Nokia6.jpg");
+     JButton button6 = new JButton("");
+     button6.setIcon(image6);
+     button6.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		CreatingNumber("6");
+     	}
+     });
+     p.setLayout(null);
+     button6.setBounds(170,350,60,50);
+     p.add(button6);
+     
+	  
+ImageIcon image7 = new ImageIcon("C:/Users/tomer/Documents/Nokia7.jpg");
+     JButton button7 = new JButton("");
+     button7.setIcon(image7);
+     button7.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		CreatingNumber("7");
+     	}
+     });
+     p.setLayout(null);
+     button7.setBounds(50,400,60,50);
+     p.add(button7);
+     	  
+		  
+ImageIcon image8 = new ImageIcon("C:/Users/tomer/Documents/Nokia8.jpg");
+     JButton button8 = new JButton("");
+     button8.setIcon(image8);
+     button8.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		CreatingNumber("8");
+     	}
+     });
+     p.setLayout(null);
+     button8.setBounds(110,400,60,50);
+     p.add(button8);
+
+
+ImageIcon image9 = new ImageIcon("C:/Users/tomer/Documents/Nokia9.jpg");
+     JButton button9 = new JButton("");
+     button9.setIcon(image9);
+     button9.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		CreatingNumber("9");
+     	}
+     });
+     p.setLayout(null);
+     button9.setBounds(170,400,60,50);
+     p.add(button9);
+	  
+	  
+ImageIcon image0 = new ImageIcon("C:/Users/tomer/Documents/Nokia0.jpg");
+     JButton button0 = new JButton("");
+     button0.setIcon(image0);
+     button0.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		CreatingNumber("0");
+     	}
+     });
+     p.setLayout(null);
+     button0.setBounds(110,450,60,50);
+     p.add(button0);
+     
+     
+ImageIcon imageHash = new ImageIcon("C:/Users/tomer/Documents/Nokia#.jpg");
+     JButton buttonHash = new JButton("");
+     buttonHash.setIcon(imageHash);
+     buttonHash.addActionListener(new ActionListener() {
+     	public void actionPerformed(ActionEvent e) {
+     		CreatingNumber("#");
+     	}
+     });
+     buttonHash.setBounds(170, 450, 60, 50);
+     p.add(buttonHash);
+     
+     
+ImageIcon imageStar = new ImageIcon("C:/Users/tomer/Documents/NokiaStar.png");
+     JButton buttonStar = new JButton("");
+     buttonStar.setIcon(imageStar);
+     buttonStar.addActionListener(new ActionListener() {
+     		public void actionPerformed(ActionEvent e) {
+     			CreatingNumber("*");
+         	}
+     	}
+     );
+     
+     buttonStar.setBounds(50, 450, 60, 50);
+     p.add(buttonStar);
+     
+     
+     JButton buttonUP = new JButton("↑");
+     buttonUP.setBounds(113, 205, 50, 30);
+     p.add(buttonUP);
+     
+     
+     JButton buttonDOWN = new JButton("↓");
+     buttonDOWN.setBounds(113, 265, 50, 30);
+     p.add(buttonDOWN);
+     
+     
+     JButton buttonLEFT = new JButton("←");
+     buttonLEFT.setBounds(90, 235, 50, 30);
+     p.add(buttonLEFT);
+     
+     
+     JButton buttonRIGHT = new JButton("→");
+     buttonRIGHT.setBounds(136, 235, 50, 30);
+     p.add(buttonRIGHT);
+     
+     
+     ImageIcon imageRight = new ImageIcon("C:/Users/tomer/Documents/NokiaMenuRight.jpg");
+     JButton MenuRight = new JButton("");
+     MenuRight.addActionListener(new ActionListener() {
+     	public void actionPerformed(ActionEvent e) {
+     	}
+     });
+     MenuRight.setIcon(imageRight);
+     MenuRight.addMouseListener(new MouseAdapter() {
+     	@Override
+     	public void mouseClicked(MouseEvent e) {
+     		if (s != null && s.length() > 0) {
+     	        s = s.substring(0, s.length() - 1);
+     	        textField.setText(s);
+     	        if(s.length() == 0) {
+     	        	PhoneState = State.Idle;
+     	        	textArea.setText(PhoneState.toString());
+     	        }
+     	    }
+     	}
+     });
+     p.setLayout(null);
+     MenuRight.setBounds(200, 205, 60, 30);
+     p.add(MenuRight);
+     
+     
+     ImageIcon imageLeft = new ImageIcon("C:/Users/tomer/Documents/NokiaMenuLeft.jpg");
+     JButton MenuLeft = new JButton("");
+     MenuLeft.setIcon(imageLeft);
+     p.setLayout(null);
+     MenuLeft.setBounds(20, 205, 60, 30);
+     p.add(MenuLeft);
+    
+     
+     JButton btnSms = new JButton("SMS");
+     btnSms.addActionListener(new ActionListener() {
+     	public void actionPerformed(ActionEvent arg0) {
+     		smsSender.NewScreen();
+     	}
+     });
+     btnSms.setBounds(270, 156, 152, 35);
+     p.add(btnSms);
+     
+     getContentPane().add(p);
+     
+     
       //PORT 
       JComboBox<String> comboBox = new JComboBox<String>();
-      comboBox.setBounds(270, 108, 284, 35);
+      comboBox.setBounds(270, 76, 284, 35);
       for(int i = 0; i<strarr.length; i++) {
     	comboBox.addItem(strarr[i].getDescriptivePortName());  
       }
@@ -178,16 +287,42 @@ public Window2(){
       		index = comboBox.getSelectedIndex();
       		PortChosen = strarr[index];
       		PortChosen.openPort();
+      		Answer.setEnabled(true);
       		textArea.setText("Opening port " + PortChosen.getDescriptivePortName());
       		btnNewButton.setEnabled(false);
+      		
+      		PortChosen.addDataListener(new SerialPortDataListener() {
+    			@Override
+    			 public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
+    			 @Override
+    			public void serialEvent(SerialPortEvent event)
+    				{
+    					if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+    					 return;
+    					if (PortChosen.bytesAvailable() > 0)
+    					{
+    						byte[] newData = new byte[PortChosen.bytesAvailable()];
+    						int numRead = PortChosen.readBytes(newData, newData.length);
+    				 //	  System.out.println("Read " + numRead + " bytes.");
+    						 if (numRead > 0)
+    						 {
+    				  //	  System.out.print(new String(newData));
+    						textArea_Debug.append(new String(newData));
+    						
+    							}			    	  
+    						 }
+    				 }
+    			});
       	}
       });
-      btnNewButton.setBounds(270, 154, 141, 23);
+      btnNewButton.setBounds(270, 122, 141, 23);
       p.add(btnNewButton);
       
-      //CALL CALL
+       
+      //CALL 
       ImageIcon imageCall = new ImageIcon("C:/Users/tomer/Documents/NokiaAnswer.jpg");
-      JButton Answer = new JButton("");
+      Answer = new JButton("");
+      Answer.setEnabled(false);
       Answer.addActionListener(new ActionListener() {
       	public void actionPerformed(ActionEvent arg0) {
       	}
@@ -222,6 +357,7 @@ public Window2(){
       Answer.setBounds(20, 240, 60, 30);
       p.add(Answer);
       
+      
       //END CALL 
       ImageIcon imageEnd = new ImageIcon("C:/Users/tomer/Documents/NokiaEnd.jpg");
       JButton HangUp = new JButton("");
@@ -230,7 +366,7 @@ public Window2(){
       	public void actionPerformed(ActionEvent arg0) {
       		String end = "ATH\r";
       		byte[] bytearrend = end.getBytes(); 
-      		strarr[index].writeBytes(bytearrend, bytearrend.length);
+      		PortChosen.writeBytes(bytearrend, bytearrend.length);
       		State Idle;
       	}
       });
@@ -247,96 +383,18 @@ public Window2(){
       HangUp.setBounds(200, 240, 60, 30);
       p.add(HangUp);
       
-      ImageIcon imageLeft = new ImageIcon("C:/Users/tomer/Documents/NokiaMenuLeft.jpg");
-      JButton MenuLeft = new JButton("");
-      MenuLeft.setIcon(imageLeft);
-      p.setLayout(null);
-      MenuLeft.setBounds(20, 205, 60, 30);
-      p.add(MenuLeft);
-      
-      ImageIcon imageRight = new ImageIcon("C:/Users/tomer/Documents/NokiaMenuRight.jpg");
-      JButton MenuRight = new JButton("");
-      MenuRight.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent e) {
-      	}
-      });
-      MenuRight.setIcon(imageRight);
-      MenuRight.addMouseListener(new MouseAdapter() {
-      	@Override
-      	public void mouseClicked(MouseEvent e) {
-      		if (s != null && s.length() > 0) {
-      	        s = s.substring(0, s.length() - 1);
-      	        textField.setText(s);
-      	        if(s.length() == 0) {
-      	        	PhoneState = State.Idle;
-      	        }
-      	    }
-      	}
-      });
-      p.setLayout(null);
-      MenuRight.setBounds(200, 205, 60, 30);
-      p.add(MenuRight);
       
       textField = new JTextField();
       textField.setBounds(20, 30, 240, 164);
       p.add(textField);
       textField.setColumns(10);
       
-      JButton buttonUP = new JButton("↑");
-      buttonUP.setBounds(113, 205, 50, 30);
-      p.add(buttonUP);
-      
-      JButton buttonDOWN = new JButton("↓");
-      buttonDOWN.setBounds(113, 265, 50, 30);
-      p.add(buttonDOWN);
-      
-      JButton buttonLEFT = new JButton("←");
-      buttonLEFT.setBounds(90, 235, 50, 30);
-      p.add(buttonLEFT);
-      
-      JButton buttonRIGHT = new JButton("→");
-      buttonRIGHT.setBounds(136, 235, 50, 30);
-      p.add(buttonRIGHT);
-      
-      ImageIcon imageHash = new ImageIcon("C:/Users/tomer/Documents/Nokia#.jpg");
-      JButton buttonHash = new JButton("");
-      buttonHash.setIcon(imageHash);
-      buttonHash.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent e) {
-      		CreatingNumber("#");
-      	}
-      });
-      buttonHash.setBounds(170, 450, 60, 50);
-      p.add(buttonHash);
-      
-      ImageIcon imageStar = new ImageIcon("C:/Users/tomer/Documents/NokiaStar.png");
-      JButton buttonStar = new JButton("");
-      buttonStar.setIcon(imageStar);
-      buttonStar.addActionListener(new ActionListener() {
-      		public void actionPerformed(ActionEvent e) {
-      			CreatingNumber("*");
-          	}
-      	}
-      );
-      
-      buttonStar.setBounds(50, 450, 60, 50);
-      p.add(buttonStar);
-      
-      JButton btnSms = new JButton("SMS");
-      btnSms.addActionListener(new ActionListener() {
-      	public void actionPerformed(ActionEvent arg0) {
-      		smsSender.NewScreen();
-      	}
-      });
-      btnSms.setBounds(270, 188, 152, 35);
-      p.add(btnSms);
-      
       //setLayout(null);
       setDefaultCloseOperation(3);
       setSize(580,600);
       setVisible(true);
 
-     }
+     } //END OF WINDOW2
 	
 public static void CreatingNumber(String n) {
 	 s = s + n;
