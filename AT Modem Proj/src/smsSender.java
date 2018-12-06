cf0897371228f6a87901891683de314ce4079b76
tomer <tomer@DESKTOP-OH6ANAJ>
2018-12-06 16:39:00 +0200

    Fully working SMS sender

--- AT Modem Proj/src/smsSender.java
+++ AT Modem Proj/src/smsSender.java
@@ -11,14 +11,15 @@ import java.awt.event.ActionEvent;
 
 public class smsSender {
 
-	private SerialPort[] strarr = SerialPort.getCommPorts();
+	//private SerialPort[] strarr = SerialPort.getCommPorts();
+	//private int index = 0;
+	private SerialPort SP = Window2.getSP();
 	private JFrame frame;
-	private int index = 0;
 	private JTextField textField;
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
+	public static void NewScreen() {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -44,35 +45,36 @@ public class smsSender {
 	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JTextArea textArea = new JTextArea();
-		textArea.setBounds(236, 144, 198, 28);
+		textArea.setBounds(226, 43, 198, 28);
 		frame.getContentPane().add(textArea);
 		
-		JComboBox<String> comboBox = new JComboBox<String>();
-		comboBox.setToolTipText("Select COM");
-		comboBox.setBounds(236, 43, 198, 35);
-		for(int i = 0; i<strarr.length; i++) {
+		/*
+		 *  JComboBox<String> comboBox = new JComboBox<String>();
+			comboBox.setToolTipText("Select COM");
+			comboBox.setBounds(236, 43, 198, 35);
+			for(int i = 0; i<strarr.length; i++) {
 	    	comboBox.addItem(strarr[i].getDescriptivePortName());  
 	      }
-	      frame.getContentPane().add(comboBox);
+	      	frame.getContentPane().add(comboBox);
 	      
-	      JButton btnNewButton = new JButton("Open Port");
-	      btnNewButton.addActionListener(new ActionListener() {
+	      	JButton btnNewButton = new JButton("Open Port");
+	      	btnNewButton.addActionListener(new ActionListener() {
 	      	public void actionPerformed(ActionEvent arg0) {
-	      		index = comboBox.getSelectedIndex();
-	      		strarr[index].openPort();
-	      		textArea.setText("Opening port " + strarr[index].getDescriptivePortName());
-	      		btnNewButton.setEnabled(false);
+	      	index = comboBox.getSelectedIndex();
+	     	strarr[index].openPort();
+	      	textArea.setText("Opening port " + strarr[index].getDescriptivePortName());
+	      	btnNewButton.setEnabled(false);
 	      	}
 	      });
-	      btnNewButton.setBounds(269, 89, 141, 23);
-	      frame.getContentPane().add(btnNewButton);
-	      
-		frame.getContentPane().add(comboBox);
-		
+	        btnNewButton.setBounds(269, 89, 141, 23);
+	      	frame.getContentPane().add(btnNewButton);
+		  	frame.getContentPane().add(comboBox);
+		  
+		 */
 		textField = new JTextField();
 		textField.setBounds(10, 43, 198, 129);
 		frame.getContentPane().add(textField);
@@ -81,27 +83,29 @@ public class smsSender {
 		JButton btnNewButton_1 = new JButton("Send Text");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				String SMS = "AT+CMGS=" + "\"0525246585\"" + "\r";
+				String Num = Window2.getCallnum();
+				String SMS = "AT+CMGS=" + "\"" +Num + "\"" + "\r";
+				textArea.setText(Num);
 	      		byte[] bytearr = SMS.getBytes();	
 	      		//index = 3;
-	      		strarr[index].writeBytes(bytearr, bytearr.length);
+	      		SP.writeBytes(bytearr, bytearr.length);
 	      		textArea.setText(SMS);
 	      		
 	      		try {
-					TimeUnit.SECONDS.sleep(1);
+					TimeUnit.SECONDS.sleep(2);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
 	      		
 	      		String MSG = textField.getText();
 	        	byte[] bytearr2 = MSG.getBytes();
-	      		strarr[index].writeBytes(bytearr2, bytearr2.length);
+	        	SP.writeBytes(bytearr2, bytearr2.length);
 	      		textArea.setText(MSG);
 	      		
 	      		byte[] bytearr3 = new byte[1];
 	      		bytearr3[0] = 26;
-	      		strarr[index].writeBytes(bytearr3, bytearr3.length);
-	    		textArea.setText("Completed");
+	      		SP.writeBytes(bytearr3, bytearr3.length);
+	    		textArea.setText("Sending a text to " + Num);
 			}
 		});
 		btnNewButton_1.setBounds(10, 179, 100, 23);
